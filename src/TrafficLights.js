47cae3da8baac72ecba5c86bc54b60425abb0dc0
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-01-17 04:31:39 -0300

    using-hook

--- src/TrafficLights.js
+++ src/TrafficLights.js
@@ -1,41 +1,42 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./TrafficLights.css";
 
-class TrafficLights extends Component {
+const TrafficLights = () => {
+  const [color, setColor] = useState({
+    clicked: null,
+  });
 
-  constructor() {
-    super();
-    this.state = {
-      clicked: null
-    };
-  }
+  let redState = "";
+  let yellowState = "";
+  let greenState = "";
 
+  if (color.clicked === "redlight") redState = "glowy";
+  if (color.clicked === "yellowlight") yellowState = "glowy";
+  if (color.clicked === "greenlight") greenState = "glowy";
 
-  render() { 
-    
-    let redState = '';
-    let yellowState = '';
-    let greenState = '';
-    if (this.state.clicked === 'redlight') redState = 'glowy';
-    if (this.state.clicked === 'yellowlight') yellowState = 'glowy';
-    if (this.state.clicked === 'greenlight') greenState = 'glowy';
-    
-    
-    return (
+  return (
     <div className="container">
-        <div className="boxClub"></div>
+      <div className="boxClub"></div>
       <div className="row">
         <div className="col d-flex justify-content-center mt-5">
           <div className="box">
-            <div className={"redlight "+redState} onClick={() => this.setState({clicked: 'redlight'})}></div>
-            <div className={"yellowlight "+yellowState} onClick={() => this.setState({clicked: 'yellowlight'})}></div>
-            <div className={"greenlight "+greenState} onClick={() => this.setState({clicked: 'greenlight'})}></div>
+            <div
+              className={"redlight " + redState}
+              onClick={() => setColor({ clicked: "redlight" })}
+            ></div>
+            <div
+              className={"yellowlight " + yellowState}
+              onClick={() => setColor({ clicked: "yellowlight" })}
+            ></div>
+            <div
+              className={"greenlight " + greenState}
+              onClick={() => setColor({ clicked: "greenlight" })}
+            ></div>
           </div>
         </div>
       </div>
     </div>
   );
-}
 };
 
 export default TrafficLights;
